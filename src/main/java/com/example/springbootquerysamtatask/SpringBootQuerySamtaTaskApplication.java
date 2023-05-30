5a316bab42c0cdced4c6d5afdb9967c0c6229c4c
Shivakant6387 <shivakant5875@gmail.com>
2023-05-30 14:30:40 +0530

    nothing to add

---
+++ src/main/java/com/example/springbootquerysamtatask/SpringBootQuerySamtaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootquerysamtatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuerySamtaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuerySamtaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootquerysamtatask/SpringBootQuerySamtaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootquerysamtatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQuerySamtaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
